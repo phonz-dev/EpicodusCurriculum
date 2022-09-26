3ff6f978125aac968f5cad7dae8c8d0a17206e94
Alphonzo Escolar <phonz.dev@gmail.com>
2022-09-26 15:17:00 +0800

    Tests passed

--- WeekOne/Thursday/Scrabble.Solution/Scrabble.Tests/ModelTests/Scrabble.Tests.cs
+++ WeekOne/Thursday/Scrabble.Solution/Scrabble.Tests/ModelTests/Scrabble.Tests.cs
@@ -10,6 +10,45 @@ namespace Scrabble.Tests
     public void ScrabbleScorerConstructor_CreatesInstanceOfScrabbleScorer_ScrabbleScorer()
     {
       ScrabbleScorer newScrabble = new ScrabbleScorer();
+      Assert.AreEqual(typeof(ScrabbleScorer), newScrabble.GetType());
+    }
+
+    [TestMethod]
+    public void GetLetterPoint_ReturnsLetterPoint_Int()
+    {
+      ScrabbleScorer scrabbleScorer = new ScrabbleScorer();
+
+      int result1 = scrabbleScorer.GetLetterPoint('A');
+      int result2 = scrabbleScorer.GetLetterPoint('D');
+      int result3 = scrabbleScorer.GetLetterPoint('B');
+      int result4 = scrabbleScorer.GetLetterPoint('F');
+      int result5 = scrabbleScorer.GetLetterPoint('K');
+      int result6 = scrabbleScorer.GetLetterPoint('J');
+      int result7 = scrabbleScorer.GetLetterPoint('Q');
+
+      Assert.AreEqual(1, result1);
+      Assert.AreEqual(2, result2);
+      Assert.AreEqual(3, result3);
+      Assert.AreEqual(4, result4);
+      Assert.AreEqual(5, result5);
+      Assert.AreEqual(8, result6);
+      Assert.AreEqual(10, result7);
+    }
+
+    [TestMethod]
+    public void CalcuateScore_CalculatesScoreOfWord_Int()
+    {
+      ScrabbleScorer scrabbleScorer = new ScrabbleScorer();
+      string word1 = "APPLE";
+      string word2 = "ZEBRA";
+      int word1Score = 9;
+      int word2Score = 16;
+
+      int score01 = scrabbleScorer.CalculateScore(word1);
+      int score02 = scrabbleScorer.CalculateScore(word2);
+
+      Assert.AreEqual(word1Score, score01);
+      Assert.AreEqual(word2Score, score02);
     }
   }
 }
--- WeekOne/Thursday/Scrabble.Solution/Scrabble/Models/Scrabble.cs
+++ WeekOne/Thursday/Scrabble.Solution/Scrabble/Models/Scrabble.cs
@@ -1,7 +1,35 @@
+using System.Collections.Generic;
+
 namespace Scrabble.Models
 {
   public class ScrabbleScorer
   {
+    public int CalculateScore(string word)
+    {
+      int total = 0;
 
+      foreach (char letter in word)
+        total += GetLetterPoint(letter);
+
+      return total;
+    }
+
+    public int GetLetterPoint(char letter)
+    {
+      List<char> onePointLetters = new List<char> { 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' };
+      List<char> twoPointLetters = new List<char> { 'D', 'G' };
+      List<char> threePointLetters = new List<char> { 'B', 'C', 'M', 'P' };
+      List<char> fourPointLetters = new List<char> { 'F', 'H', 'V', 'W', 'Y' };
+      List<char> eightPointLetters = new List<char> { 'J', 'X' };
+
+      if (letter == 'K') return 5;
+      if (onePointLetters.Contains(letter)) return 1;
+      if (twoPointLetters.Contains(letter)) return 2;
+      if (threePointLetters.Contains(letter)) return 3;
+      if (fourPointLetters.Contains(letter)) return 4;
+      if (eightPointLetters.Contains(letter)) return 8;
+
+      return 10;
+    }
   }
 }
